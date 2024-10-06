08a0cfd32ca94195192b33dee61c0c26d35c87c9
apoloniaabramowicz <pola.abramowicz@onet.pl>
2024-10-06 03:14:21 +0200

    some improvements

--- src/components/IngredientsList.tsx
+++ src/components/IngredientsList.tsx
@@ -5,8 +5,6 @@ import {
     Carousel,
     CarouselContent,
     CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
 } from "@/components/ui/carousel"
 import {Ingredient} from "@/lib/types";
 
